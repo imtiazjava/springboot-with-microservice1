7f8801335e0635e52a4faf436487185e60bf7e6d
imtiaz <imtiazjavatrainer@gmail.com>
2022-11-17 16:15:22 +0530

    addedspringcloudconfig

---
+++ spring-cloud-cofig/src/test/java/com/cgi/cloudconfig/SpringCloudCofigApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cgi.cloudconfig;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudCofigApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
